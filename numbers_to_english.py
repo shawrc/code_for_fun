3491fb5f6c5a5d0abb45e437a61819b1956615e8
Ryan Shaw <shawryc@gmail.com>
2015-09-17 14:32:54 -0700

    Make code look better.

--- numbers_to_english.py
+++ numbers_to_english.py
@@ -1,71 +1,44 @@
 """Given an int32 number, print it in English."""
 def int_to_en(num):
-    m = { 0 : 'zero',
-          1 : 'one',
-          2 : 'two',
-          3 : 'three',
-          4 : 'four',
-          5 : 'five',
-          6 : 'six',
-          7 : 'seven',
-          8 : 'eight',
-          9 : 'nine',
-          10 : 'ten',
-          11 : 'eleven',
-          12 : 'twelve',
-          13 : 'thirteen',
-          14 : 'fourteen',
-          15 : 'fifteen',
-          16 : 'sixteen',
-          17 : 'seventeen',
-          18 : 'eighteen',
-          19 : 'ninteen',
-          20 : 'twenty',
-          30 : 'thirty',
-          40 : 'fourth',
-          50 : 'fifty',
-          60 : 'sixty',
-          70 : 'seventy',
-          80 : 'eighty',
-          90 : 'ninty',
-          100 : 'hundred',
-          1000 : 'thousand',
-          1000000 : 'million',
-          1000000000 : 'billion' }
+    d = { 0 : 'zero', 1 : 'one', 2 : 'two', 3 : 'three', 4 : 'four', 5 : 'five',
+          6 : 'six', 7 : 'seven', 8 : 'eight', 9 : 'nine', 10 : 'ten',
+          11 : 'eleven', 12 : 'twelve', 13 : 'thirteen', 14 : 'fourteen',
+          15 : 'fifteen', 16 : 'sixteen', 17 : 'seventeen', 18 : 'eighteen',
+          19 : 'ninteen', 20 : 'twenty',
+          30 : 'thirty', 40 : 'fourth', 50 : 'fifty', 60 : 'sixty',
+          70 : 'seventy', 80 : 'eighty', 90 : 'ninty' }
+    k = 1000
+    m = k * 1000
+    b = m * 1000
+    t = b * 1000
 
-    if (num < 20):
-        return m[num]
-    # 20 <= num and num < 100
-    if (num < 100):
-        if num % 10 == 0:
-            return m[num]
-        else:
-            return m[num // 10 * 10] + '-' + m[num % 10]
-    # 100 <= num and num < 1000
-    if (num < 1000):
-        if num % 100 == 0:
-            return m[num // 100] + ' hundred '
-        else:
-            return m[num // 100] + ' hundred and ' + int_to_en(num % 100)
-    # 1000 <= num and num < 1000_000
-    if (num < 1000000):
-        if num % 1000 == 0:
-            return int_to_en(num // 1000) + ' thousand '
-        else:
-            return int_to_en(num // 1000) + ' thousand ' + int_to_en(num % 1000)
-    # 1_000_000 < num and num < 1000_000_000
-    if (num < 1000000000):
-        if (num % 1000000) == 0:
-            return int_to_en(num // 1000000) + ' million '
-        else:
-            return int_to_en(num // 1000000) + ' million ' + int_to_en(num % 1000000)
+    assert(0 <= num)
 
-    if (num % 1000000000) == 0:
-        return int_to_en(num // 1000000000) + ' billion '
-    else:
-        return int_to_en(num // 1000000000) + ' billion ' + int_to_en(num % 1000000000)
+    if (num < 20): return d[num]
+    if (20 <= num < 100):
+        if num % 10 == 0: return d[num]
+        else: return d[num // 10 * 10] + '-' + d[num % 10]
 
-    assert(False)
+    if (num < k):
+        if num % 100 == 0: return d[num // 100] + ' hundred'
+        else: return d[num // 100] + ' hundred and ' + int_to_en(num % 100)
+
+    if (num < m):
+        if num % k == 0: return int_to_en(num // k) + ' thousand'
+        else: return int_to_en(num // k) + ' thousand ' + int_to_en(num % k)
+
+    if (num < b):
+        if (num % m) == 0: return int_to_en(num // m) + ' million'
+        else: return int_to_en(num // m) + ' million ' + int_to_en(num % m)
+
+    if (num < t):
+        if (num % b) == 0: return int_to_en(num // b) + ' billion'
+        else: return int_to_en(num // b) + ' billion ' + int_to_en(num % b)
+
+    if (num % t == 0): return int_to_en(num // t) + ' trillion'
+    else: return int_to_en(num // t) + ' trillion ' + int_to_en(num % t)
+
+    raise AssertionError('num is too large: %s' % str(num))
 
 print(0, int_to_en(0))
 print(3, int_to_en(3))
